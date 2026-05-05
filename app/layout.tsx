4f306ea53a7d9b7b44059dfb0009a36f723963ce
vonnue-niju <niju.siyadh@vonnue.com>
2026-05-05 15:58:12 +0530

    chore: prettier configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+   title: 'Create Next App',
 };
 
 export default function RootLayout({
-  children,
+   children,
 }: Readonly<{
-  children: React.ReactNode;
+   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <body className="min-h-full flex flex-col">{children}</body>
+      </html>
+   );
 }
